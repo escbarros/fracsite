e782fcc141d2035a1b4969a8b02a110392524e9e
Eduardo <eduardoscbarros@hotmail.com>
2022-05-31 21:15:30 -0300

    muitas coisas

--- app/assets/js/navbar.js
+++ app/assets/js/navbar.js
@@ -1,19 +1,20 @@
-function mudaTamanhoNav(){
-    navbarHeight = document.querySelector(".navbar").offsetHeight
-    document.querySelector("main").style.marginTop = `${navbarHeight}px` 
-}
+// function mudaTamanhoNav(){
+//     navbarHeight = document.querySelector(".navbar").offsetHeight
+//     document.querySelector("main").style.marginTop = `${navbarHeight}px` 
+// }
 var menuOpen = false;
 var navbarHeight = document.querySelector(".navbar").offsetHeight
-mudaTamanhoNav()
+// mudaTamanhoNav()
 document.querySelector(".navbar-hamburger").addEventListener("click", ()=>{
+    console.log("a");
     menuOpen ? document.querySelector(".navbar").classList.remove("open") : document.querySelector(".navbar").classList.add("open");
     menuOpen = !menuOpen
-    mudaTamanhoNav()
+    // mudaTamanhoNav()
 })
 document.querySelector('main').addEventListener("click", ()=>{
     console.log("teste");
     if(menuOpen){
         document.querySelector(".navbar").classList.remove("open")
-        mudaTamanhoNav()
+        // mudaTamanhoNav()
     } 
 })
