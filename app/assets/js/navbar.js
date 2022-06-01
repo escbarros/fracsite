f52b49511e1ddee9374ce79461d8c13d731722a6
Eduardo <eduardoscbarros@hotmail.com>
2022-06-01 00:32:28 -0300

    feito a versao mobile

--- app/assets/js/navbar.js
+++ app/assets/js/navbar.js
@@ -5,9 +5,17 @@
 var menuOpen = false;
 var navbarHeight = document.querySelector(".navbar").offsetHeight
 // mudaTamanhoNav()
-document.querySelector(".navbar-hamburger").addEventListener("click", ()=>{
+document.querySelector("#botaoMenu").addEventListener("click", ()=>{
     console.log("a");
-    menuOpen ? document.querySelector(".navbar").classList.remove("open") : document.querySelector(".navbar").classList.add("open");
+    if(!menuOpen){
+        document.querySelector(".navbar").classList.add("open")
+        document.querySelector("#botaoMenu").classList.remove("fa-bars")
+        document.querySelector("#botaoMenu").classList.add("fa-xmark")
+    }else{
+        document.querySelector(".navbar").classList.remove("open")
+        document.querySelector("#botaoMenu").classList.add("fa-bars")
+        document.querySelector("#botaoMenu").classList.remove("fa-xmark")
+    }
     menuOpen = !menuOpen
     // mudaTamanhoNav()
 })
@@ -15,6 +23,8 @@ document.querySelector('main').addEventListener("click", ()=>{
     console.log("teste");
     if(menuOpen){
         document.querySelector(".navbar").classList.remove("open")
+        document.querySelector("#botaoMenu").classList.add("fa-bars")
+        document.querySelector("#botaoMenu").classList.remove("fa-xmark")
         // mudaTamanhoNav()
     } 
 })
