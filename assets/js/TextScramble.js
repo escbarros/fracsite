8a3ffd6b1d85498ab9d640844cf38a31c51af744
Eduardo Barros <69276792+escbarros@users.noreply.github.com>
2022-06-04 01:40:40 -0300

    adicionado os cursos

--- assets/js/TextScramble.js
+++ assets/js/TextScramble.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Feito por Justin Windle
 // https://codepen.io/soulwire/pen/mEMPrK
 
@@ -71,4 +72,79 @@ const proxFrase = ()=>{
     })
     contador = (contador + 1) % frases.length
 }
+=======
+// Feito por Justin Windle
+// https://codepen.io/soulwire/pen/mEMPrK
+
+class TextScramble {
+    constructor(el) {
+        this.el = el
+        this.chars = '!<>-_\\/[]{}—=+*^?#________'
+        this.update = this.update.bind(this)
+    }
+    setText(newText) {
+        const oldText = this.el.innerText
+        const length = Math.max(oldText.length, newText.length)
+        const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = []
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || ''
+            const to = newText[i] || ''
+            const start = Math.floor(Math.random() * 40)
+            const end = start + Math.floor(Math.random() * 40)
+            this.queue.push({ from, to, start, end })
+        }
+        cancelAnimationFrame(this.frameRequest)
+        this.frame = 0
+        this.update()
+        return promise
+    }
+    update() {
+        let output = ''
+        let complete = 0
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i]
+            if (this.frame >= end) {
+                complete++
+                output += to
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                char = this.randomChar()
+                this.queue[i].char = char
+                }
+                output += `<span class="dud">${char}</span>`
+            } else {
+                output += from
+            }
+        }
+        this.el.innerHTML = output
+        if (complete === this.queue.length) {
+            this.resolve()
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update)
+            this.frame++
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+}
+const frases = [
+    'Olá mundo',
+    'Hello World',
+    'こんにちは世界',
+    'Hallo Welt',
+    'مرحبا بالعالم',
+]
+const elemento = document.querySelector(".landing_conteudo-titulo");
+const efeito = new TextScramble(elemento);
+
+let contador = 0
+const proxFrase = ()=>{
+    efeito.setText(frases[contador]).then(()=>{
+        setTimeout(proxFrase, 5000)
+    })
+    contador = (contador + 1) % frases.length
+}
+>>>>>>> f5a7e8dda496030487b13282a8b76908184af082
 proxFrase()
